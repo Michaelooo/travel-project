748b568b2f10a7da786cdea0556beefa0dd2ad0b
Pengfei Cheng <Pengfei.Cheng@symbio.com>
2017-04-27 17:54:58 +0800

    详情页

--- js/scroll.js
+++ js/scroll.js
@@ -1,52 +1,53 @@
-// 自执行方法，给$绑定一个滚动方法
-(function($){
-	$.fn.myScroll = function(options){
-	var defaults = {
-		speed:40,  
-		rowHeight:24 
-	};
-	
-	var opts = $.extend({}, defaults, options),intId = [];
-	
-	function marquee(obj, step){
-	
-		obj.find("ul").animate({
-			marginTop: '-=1'
-		},0,function(){
-				var s = Math.abs(parseInt($(this).css("margin-top")));
-				if(s >= step){
-					$(this).find("li").slice(0, 1).appendTo($(this));
-					$(this).css("margin-top", 0);
-				}
-			});
-		}
-		
-		this.each(function(i){
-			var sh = opts["rowHeight"],speed = opts["speed"],_this = $(this);
-			intId[i] = setInterval(function(){
-				if(_this.find("ul").height()<=_this.height()){
-					clearInterval(intId[i]);
-				}else{
-					marquee(_this, sh);
-				}
-			}, speed);
+// 扩展方法，用于文字滚动
+(function($) {
+    $.fn.myScroll = function(options) {
+        var defaults = {
+            speed: 40,
+            rowHeight: 24
+        };
 
-			_this.hover(function(){
-				clearInterval(intId[i]);
-			},function(){
-				intId[i] = setInterval(function(){
-					if(_this.find("ul").height()<=_this.height()){
-						clearInterval(intId[i]);
-					}else{
-						marquee(_this, sh);
-					}
-				}, speed);
-			});
-		
-		});
+        var opts = $.extend({}, defaults, options),
+            intId = [];
 
-	}
+        //	滚定私有方法，共有两个参数，第一个参数为滚对对象，第二个为行高
+        function _scroll(obj, step) {
+            obj.find("ul").animate({
+                marginTop: '-=1'
+            }, 0, function() {
+                var s = Math.abs(parseInt($(this).css("margin-top")));
+                if (s >= step) {
+                    $(this).find("li").slice(0, 1).appendTo($(this));
+                    $(this).css("margin-top", 0);
+                }
+            });
+        };
 
+        this.each(function(i) {
+            var rowHeight = opts["rowHeight"],
+                speed = opts["speed"],
+                _this = $(this);
+            intId[i] = setInterval(function() {
+                // 要给外边的盒子设置宽度
+                if (_this.find("ul").height() <= _this.height()) {
+                    clearInterval(intId[i]);
+                } else {
+                    _scroll(_this, rowHeight);
+                }
+            }, speed);
+
+            _this.hover(function() {
+                clearInterval(intId[i]);
+            }, function() {
+                intId[i] = setInterval(function() {
+                    if (_this.find("ul").height() <= _this.height()) {
+                        clearInterval(intId[i]);
+                    } else {
+                        _scroll(_this, rowHeight);
+                    }
+                }, speed);
+            });
+        });
+    }
 })(jQuery);
 
 
@@ -63,10 +64,20 @@
  * speed，表示滚动速度，数字越大表示越慢，
  * rowHeight，表示滚动行的高度
  */
-$(function(){
-	$("div.list_lh").myScroll({
-		speed:40, 
-		rowHeight:600
-	});
+$(function() {
+    $("div.scrollText").myScroll({
+        speed: 40,
+        rowHeight: 600
+    });
 });
 
+/*
+浮动效果实现
+ */
+$(function(){
+    $("#right").hover(function(){
+        $(this).animate({right: "0px"}, 200);
+    }, function(){
+        $(this).animate({right: "-180px"}, 200);
+    });
+});
