1db5671a4e6ae1bd938efef0dc4780ff7a495d38
unknown <DEV@RightIC-CPF.rightic.local>
2017-05-19 17:43:34 +0800

    第一次修改

--- publish/js/main.js
+++
@@ -1,1 +0,0 @@
-function refreshPage(){history.go(0)}$(function(){$("#slider").easySlider({slideSpeed:500,paginationSpacing:"15px",paginationDiameter:"12px",paginationPositionFromBottom:"20px",slidesClass:".slides",controlsClass:".controls",paginationClass:".pagination"})});
