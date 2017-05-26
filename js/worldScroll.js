415054c9b4cf5612f2dd8aa77da25ded022895c4
unknown <DEV@RightIC-CPF.rightic.local>
2017-05-26 15:52:34 +0800

    添加地球动画

--- js/worldScroll.js
+++ js/worldScroll.js
@@ -1,28 +1,10 @@
 // 这个脚本只用于地图滚动效果
 $(function() {
 	var $world = $('.header .left .worldIcon >a >img');
-	$world.mouseenter(function() {
-		setTimeout(function() {
-			$world.animate({
-					'left': -10,
-					'top': -10
-				},
-				700,
-				function() {
-					/* stuff to do after animation is complete */
-				});
-		}, 100)
-	})
-	$world.mouseleave(function() {
-		setTimeout(function() {
-			$world.animate({
-					'left': -30,
-					'top': -30
-				},
-				700,
-				function() {
-					/* stuff to do after animation is complete */
-				});
-		}, 100);
-	})
+	$world.hover(function(){
+		// $world.addClass('actived');
+		$world.animate({'width': 150,'height':150,'opacity':0.6}, 500)
+	},function(){
+		$world.animate({'width': 100,'height':100,'opacity':0.2}, 500)
+	});
 })
