6aa0d96971d901a1b3f5012fe788c78320fd09b5
Michael.Cheng <422201870@qq.com>
2017-06-07 09:40:21 +0800

    修复别人提的BUG

--- js/worldScroll.js
+++ js/worldScroll.js
@@ -1,12 +1,24 @@
 // 这个脚本只用于地图滚动效果
-$(function() {
-	var $world = $('.leftHeader .worldIcon >a >img.scrollWorld');
-	var $logo = $('.leftHeader .worldIcon .logo .imglogo');
-	$world.hover(function(){
-		$world.stop().animate({'width': 150,'height':150,'opacity':0.6}, 200);
-		$logo.stop().animate({'opacity':0.5}, 200);
-	},function(){
-		$world.stop().animate({'width': 100,'height':100,'opacity':0.2}, 200);
-		$logo.stop().animate({'opacity':1}, 200);
+$(function () {
+	var $world = $('.header .left .worldIcon >a >img.scrollWorld');
+	var $logo = $('.header .left .worldIcon .logo .imglogo');
+	$world.hover(function () {
+		$world.stop().animate({
+			'width': 150,
+			'height': 150,
+			'opacity': 0.6
+		}, 200);
+		$logo.stop().animate({
+			'opacity': 0.5
+		}, 200);
+	}, function () {
+		$world.stop().animate({
+			'width': 100,
+			'height': 100,
+			'opacity': 0.2
+		}, 200);
+		$logo.stop().animate({
+			'opacity': 1
+		}, 200);
 	});
 })
